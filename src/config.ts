c41ff8a42c677098a6da381debf3eb6d659e9b7b
Zachary Stout <stout.zachary@gmail.com>
2023-08-17 18:54:48 -0600

    changed config. does this still work?

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "Personal Portfolio Website Template built with Astro and TailwindCSS";
-export const SITE_DESCRIPTION =
-  "Updated my portfolio website to use Astro and TailwindCSS.";
+  "Personal Portfolio Website built with Astro and TailwindCSS";
+export const SITE_DESCRIPTION = "Updated my portfolio website to use Astro.";
