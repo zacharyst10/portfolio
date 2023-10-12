af36b30bf0166091b5fcc2a68539b88be3dddcfe
Zachary Stout <stout.zachary@gmail.com>
2023-10-12 12:47:06 -0600

    finally upgraded to astr0 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import image from "@astrojs/image";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-    cacheDir: "./.cache/image",
-    logLevel: 'debug'
-  })],
+  integrations: [mdx(), sitemap(), tailwind(),],
+ 
   output: "server",
   adapter: vercel()
 });
